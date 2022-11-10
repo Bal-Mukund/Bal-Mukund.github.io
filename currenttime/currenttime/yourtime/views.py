899ec0ddd9e00fa4454014d7b1a3853723dc0adf
Bal Mukund <balmukundbabhanlai@gmail.com>
2022-11-10 15:42:23 +0530

    Delete currenttime/currenttime directory

--- currenttime/currenttime/yourtime/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-import datetime
-
-def cr_time(request):
-    now = datetime.datetime.now()
-    now = now.ctime()
-    return render(request, 'yourtime/cr_time.html',{
-        'crtime' : now
-    })
